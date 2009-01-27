36ab275354efceae9483e4032ce7fd82ec2c2328
Wade Simmons <wsimmons@gmail.com>
2009-01-27 13:08:05 -0700

    add a function config_load for nicely loading scripts with local changes

---
+++ functions/config.zsh
@@ -0,0 +1,16 @@
+autoload colors
+colors
+
+# Load a config file named $1, and if a $1_local files exists load that too
+config_load () {
+    if [ -e "$1" ]; then
+        echo "  ${fg_bold[green]}loading    ${fg_no_bold[default]} $1"
+        source "$1"
+        if [ -e "$1_local" ]; then
+            echo "  ${fg_bold[green]}loading    ${fg_no_bold[default]} $1_local"
+            source "$1_local"
+        fi
+    else
+        echo "  ${fg_bold[red]}cannot load${fg_no_bold[default]} $1"
+    fi
+}

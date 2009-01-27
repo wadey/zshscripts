4022ddfb70a6ac3017585ebce735f6c7208040d8
Wade Simmons <wsimmons@gmail.com>
2009-01-27 12:15:16 -0700

    add two prompt function, one with dirty indicator

--- functions/git.zsh
+++ functions/git.zsh
@@ -2,11 +2,18 @@
 function parse_git_dirty {
   git diff --quiet 2>&- || echo -n "*"
 }
+
 # This method is slower, but does include untracked files
 function parse_git_unclean {
   git status 2>&- | grep -q 'working directory clean' || echo -n "*"
 }
+
 function git_prompt {
+  ref=$(git symbolic-ref HEAD 2>&-) || return
+  echo "[${ref#refs/heads/}]"
+}
+
+function git_prompt_with_dirty {
   ref=$(git symbolic-ref HEAD 2>&-) || return
   echo "[${ref#refs/heads/}$(parse_git_dirty)]"
 }
